372613e854acd669cf614a8d22080d012fb1c194
seyss51 <seyss51@gmail.com>
2026-07-19 07:50:00 +0200

    Update StockScreen.kt

--- app/src/main/java/com/julien/frigomalin/ui/screens/StockScreen.kt
+++ app/src/main/java/com/julien/frigomalin/ui/screens/StockScreen.kt
@@ -1,5 +1,6 @@
 package com.julien.frigomalin.ui.screens
 
+import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items

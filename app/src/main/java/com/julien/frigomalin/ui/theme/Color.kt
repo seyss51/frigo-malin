59873034e6bc40af04a6195b86851ff9ba2dd45e
seyss51 <seyss51@gmail.com>
2026-07-31 23:19:25 +0200

    Create app/src/main/java/com/julien/frigomalin/ui/theme/Color.kt

---
+++ app/src/main/java/com/julien/frigomalin/ui/theme/Color.kt
@@ -0,0 +1,40 @@
+package com.julien.frigomalin.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Palette claire
+val TealPrimary = Color(0xFF00796B)
+val TealOnPrimary = Color(0xFFFFFFFF)
+val TealPrimaryContainer = Color(0xFFB2DFDB)
+val TealOnPrimaryContainer = Color(0xFF002019)
+
+val CorailSecondary = Color(0xFFFF7A59)
+val CorailOnSecondary = Color(0xFFFFFFFF)
+val CorailSecondaryContainer = Color(0xFFFFDBCF)
+val CorailOnSecondaryContainer = Color(0xFF3A0900)
+
+val Tertiary = Color(0xFF4F6357)
+val Background = Color(0xFFF7FAF9)
+val OnBackground = Color(0xFF191C1B)
+val Surface = Color(0xFFF7FAF9)
+val OnSurface = Color(0xFF191C1B)
+val SurfaceVariant = Color(0xFFDBE5E1)
+val Erreur = Color(0xFFBA1A1A)
+
+// Palette sombre
+val TealPrimaryDark = Color(0xFF4DB6AC)
+val TealOnPrimaryDark = Color(0xFF003731)
+val TealPrimaryContainerDark = Color(0xFF00504A)
+val TealOnPrimaryContainerDark = Color(0xFFB2DFDB)
+
+val CorailSecondaryDark = Color(0xFFFFB59D)
+val CorailOnSecondaryDark = Color(0xFF5F1600)
+val CorailSecondaryContainerDark = Color(0xFF7D2C0F)
+val CorailOnSecondaryContainerDark = Color(0xFFFFDBCF)
+
+val BackgroundDark = Color(0xFF191C1B)
+val OnBackgroundDark = Color(0xFFE0E3E1)
+val SurfaceDark = Color(0xFF191C1B)
+val OnSurfaceDark = Color(0xFFE0E3E1)
+val SurfaceVariantDark = Color(0xFF3F4945)
+val ErreurDark = Color(0xFFFFB4AB)

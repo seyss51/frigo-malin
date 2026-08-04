63678228804450756b9ba03635b0b4b969481689
seyss51 <seyss51@gmail.com>
2026-08-04 09:22:37 +0200

    Update Migrations.kt

--- app/src/main/java/com/julien/frigomalin/data/Migrations.kt
+++ app/src/main/java/com/julien/frigomalin/data/Migrations.kt
@@ -8,3 +8,9 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         db.execSQL("ALTER TABLE recettes ADD COLUMN photoPath TEXT")
     }
 }
+
+val MIGRATION_2_3 = object : Migration(2, 3) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE recettes ADD COLUMN sourceUrl TEXT")
+    }
+}

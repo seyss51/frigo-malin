222910cbd5f03b762f9c437c0532a36840acec66
seyss51 <seyss51@gmail.com>
2026-08-15 18:09:32 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,12 @@ dependencies {
     // Photos
     implementation("io.coil-kt:coil-compose:2.6.0")
 
+    // Scan de code-barres
+    implementation("androidx.camera:camera-core:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-view:1.3.4")
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
